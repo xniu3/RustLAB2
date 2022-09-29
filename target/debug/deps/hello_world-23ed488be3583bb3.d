ab2be01c6b7a54fe3ca9487b51a506e5c0e6f11a
xinhaoniu <xniu3@ualberta.ca>
2022-09-29 15:36:41 -0600

    Clean Repository

--- target/debug/deps/hello_world-23ed488be3583bb3.d
+++ target/debug/deps/hello_world-23ed488be3583bb3.d
@@ -1,5 +1,6 @@
-/Users/xinhaoniu/Projects/RustLab1/hello_world/target/debug/deps/hello_world-23ed488be3583bb3: src/main.rs
+/Users/xinhaoniu/Projects/RustLab2/target/debug/deps/hello_world-23ed488be3583bb3: src/main.rs src/function.rs
 
-/Users/xinhaoniu/Projects/RustLab1/hello_world/target/debug/deps/hello_world-23ed488be3583bb3.d: src/main.rs
+/Users/xinhaoniu/Projects/RustLab2/target/debug/deps/hello_world-23ed488be3583bb3.d: src/main.rs src/function.rs
 
 src/main.rs:
+src/function.rs:
