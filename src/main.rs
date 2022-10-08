06a3f8ef1a34c0107b7df669d00ae8d3caa8aa41
xinhaoniu <xniu3@ualberta.ca>
2022-10-08 17:01:08 -0600

    Question 3 Solved

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,38 @@
-
 #[derive(Debug)]
-
-pub struct TreeNode<T> {
-    pub data: T,
-    pub left_child: Option<Box<TreeNode<T>>>,
-    pub right_child: Option<Box<TreeNode<T>>>,
+pub struct TreeNode<'a>{
+    data: &'a str,
+    left_child: Option<Box<TreeNode<'a>>>,
+    right_child: Option<Box<TreeNode<'a>>>
 }
 
-impl TreeNode<i32>{
-    
-    pub fn insert_node(&mut self, data:i32)  {
-
+impl <'a> TreeNode <'a>{
+    pub fn insert_node(&mut self, data: &'a str) {
         if self.data == data {
             return
         }
-
-        let new_node = if data < self.data { 
-            &mut self.left_child
-        } 
-        
-        else {
-            &mut self.right_child 
-        };
-        match new_node {
-            Some(ref mut node) => node.insert_node(data),
-            None => {
-                let node = TreeNode{data, left_child:None, right_child:None};
-                let box_node = Some(Box::new(node));
-                *new_node = box_node;
-            },
+        let new_node = 
+            if data < self.data { 
+                &mut self.left_child 
+            } 
+            else {
+                &mut self.right_child 
+            };
+        match new_node{
+            &mut Some(ref mut node) => node.insert_node(data),
+            &mut None =>{
+                let temp_node = TreeNode{data:data, left_child:None , right_child:None};
+                *new_node = Some(Box::new(temp_node));
+            }
         }
     }
 }
 
 fn main(){
-    let mut root = TreeNode{
-        data: 5,
-        left_child: None,
-        right_child: None, 
-    };
-    root.insert_node(4);
+    let mut root_val = "55";
+    let mut root = TreeNode{data:root_val, left_child: None, right_child:None};
+    root.insert_node("6");
+    root.insert_node("9");
+    root.insert_node("701");
+    root.insert_node("705");
+    println!("{:#?}",root);
 }
-
