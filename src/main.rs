c2847c1c4b9b7d49d600a0a85024e00f8a4f6d6d
xinhaoniu <xniu3@ualberta.ca>
2022-10-08 17:52:08 -0600

    Question 4 Solved

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,38 @@
-#[derive(Debug)]
-pub struct TreeNode<'a>{
-    data: &'a str,
-    left_child: Option<Box<TreeNode<'a>>>,
-    right_child: Option<Box<TreeNode<'a>>>
-}
+use crate::Tree::{Node, Empty};
 
-impl <'a> TreeNode <'a>{
-    pub fn insert_node(&mut self, data: &'a str) {
-        if self.data == data {
-            return
-        }
-        let new_node = 
-            if data < self.data { 
-                &mut self.left_child 
-            } 
-            else {
-                &mut self.right_child 
-            };
-        match new_node{
-            &mut Some(ref mut node) => node.insert_node(data),
-            &mut None =>{
-                let temp_node = TreeNode{data:data, left_child:None , right_child:None};
-                *new_node = Some(Box::new(temp_node));
+#[derive(Debug)]
+#[allow(dead_code)]
+enum Tree<T: Ord> {
+    Node {
+        data: T,
+        left_child: Box<Tree<T>>,
+        right_child: Box<Tree<T>>,
+    },
+    Empty,
+}
+fn insert_node<T:Ord>(TreeNode: Box<Tree<T>>, data: T) -> Box<Tree<T>> {
+    return Box::new(
+        match(*TreeNode){
+            Empty => Node{data:data, left_child: Box::new(Empty), right_child: Box::new(Empty),},
+            Node{data:root_data, left_child: left, right_child: right} =>
+            if data  < root_data{
+                Node{data:root_data, left_child: insert_node(left, data), right_child: right}
+            }
+            else{
+                Node{data:root_data, left_child: left, right_child: insert_node(right, data)}
             }
         }
-    }
+    );
 }
 
+
 fn main(){
-    let mut root_val = "55";
-    let mut root = TreeNode{data:root_val, left_child: None, right_child:None};
-    root.insert_node("6");
-    root.insert_node("9");
-    root.insert_node("701");
-    root.insert_node("705");
-    println!("{:#?}",root);
+    let mut root = Box::new(Node{data:716, left_child:Box::new(Empty) , right_child: Box::new(Empty)});
+
+    root = insert_node(root, 56);
+    root = insert_node(root, 705);
+    root = insert_node(root, 719);
+    root = insert_node(root, 747);
+
+    println!("{:?}",root);
 }
