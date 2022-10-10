dc39eca2c20f0e21c4b6004d121af8db5c6a7224
xinhaoniu <xniu3@ualberta.ca>
2022-10-10 14:22:51 -0600

    2

---
+++ target/debug/deps/hello_world-23ed488be3583bb3.d
@@ -0,0 +1,5 @@
+/Users/xinhaoniu/Projects/RustLab2/target/debug/deps/hello_world-23ed488be3583bb3: src/main.rs
+
+/Users/xinhaoniu/Projects/RustLab2/target/debug/deps/hello_world-23ed488be3583bb3.d: src/main.rs
+
+src/main.rs:
